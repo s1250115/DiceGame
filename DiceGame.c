93f9af591c86465b042e5a2c515a2cffd00cdbd3
菅原大佑 <sugawaradaisuke@sugawaradaisukenoMacBook-Pro.local>
2018-07-08 16:39:08 +0900

    Won/lost message modified

--- DiceGame.c
+++ DiceGame.c
@@ -5,16 +5,25 @@
 int main()
 {
   int ret1,ret2;
+  char name[20];
 
   srand(time(NULL));
 
   ret1 = 1 + rand() % 6;
   ret2 = 1 + rand() % 6;
 
+  printf("What is your name?\n");
+  scanf("%s",name);
+  printf("Hello, %s\n\n",name);
+  
   printf("Rolling the dice...\n");
   printf("Die 1: %d\n",ret1);
   printf("Die 2: %d\n",ret2);
   printf("Total value: %d\n",ret1+ret2);
+
+  if(ret1+ret2 > 7) printf("%s won!\n",name);
+  else printf("%s lost!\n",name);
+  
 
   return 0;
 }
